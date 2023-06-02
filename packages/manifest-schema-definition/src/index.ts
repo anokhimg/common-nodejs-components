463ecaea34db80a59ba32beee05ebf8ca89cc99f
EC2 Default User <ec2-user@ip-172-31-96-122.ec2.internal>
2023-06-02 07:16:27 +0000

    New changes 2023-06-02

--- packages/manifest-schema-definition/src/index.ts
+++ packages/manifest-schema-definition/src/index.ts
@@ -1,48 +1,51 @@
-import manifestVersion from './manifest/manifest-version.json';
+import Ajv from 'ajv';
+import root from './manifest/root.json';
+
 import manifestDiscoveryPort from './manifest/manifest-discoveryPort.json';
 import manifestInputPorts from './manifest/manifest-inputPort.json';
 import manifestTransformation from './manifest/manifest-transformation.json';
 import manifestProductState from './manifest/manifest-productState.json';
 import manifestOutPutPort from './manifest/manifest-outPutPort.json';
 import manifestControlPort from './manifest/manifest-controlPort.json';
-import manifestSchema from './manifest/manifestSchema.json';
-
 import dataQualityCheckChecks from './manifest/ref/controlPort/dataQualityCheckChecks.json';
 import dataQualityCheckCommon from './manifest/ref/controlPort/dataQualityCheckCommon.json';
 import dataQualityCheckExpression from './manifest/ref/controlPort/dataQualityCheckExpression.json';
 import dataQualityCheckSingleCheck from './manifest/ref/controlPort/dataQualityCheckSingleCheck.json';
-import dataSetUrn from './manifest/ref/inputPort/dataSetUrn.json';
+import connectionUrn from './manifest/ref/inputPort/connectionUrn.json';
 import incrementalStrategy from './manifest/ref/inputPort/incrementalStrategy.json';
 import s3CsvDataSet from './manifest/ref/inputPort/s3CsvDataSet.json';
 import syncType from './manifest/ref/inputPort/syncType.json';
-import inputPortTags from './manifest/ref/inputPort/tags.json';
 import alias from './manifest/ref/inputPort/alias.json';
 import dataTimeRange from './manifest/ref/discoveryPort/dataTimeRange.json';
 import regulatoryFields from './manifest/ref/discoveryPort/regulatoryFields.json';
 
-const finalManifest = {
-  manifestSchema: manifestSchema,
-  version: manifestVersion,
-  discoveryPort: manifestDiscoveryPort,
-  inputPorts: manifestInputPorts,
-  transformation: manifestTransformation,
-  productState: manifestProductState,
-  outputPort: manifestOutPutPort,
-  controlPort: manifestControlPort,
-  refs: [
-    dataTimeRange,
-    regulatoryFields,
-    dataSetUrn,
-    incrementalStrategy,
-    s3CsvDataSet,
-    syncType,
-    inputPortTags,
-    alias,
-    dataQualityCheckChecks,
-    dataQualityCheckCommon,
-    dataQualityCheckExpression,
-    dataQualityCheckSingleCheck,
-  ],
-};
+export const schema = Object.freeze(root);
+const privateRefs = [
+  manifestDiscoveryPort,
+  manifestInputPorts,
+  manifestTransformation,
+  manifestProductState,
+  manifestOutPutPort,
+  manifestControlPort,
+  dataTimeRange,
+  regulatoryFields,
+  connectionUrn,
+  incrementalStrategy,
+  s3CsvDataSet,
+  syncType,
+  alias,
+  dataQualityCheckChecks,
+  dataQualityCheckCommon,
+  dataQualityCheckExpression,
+  dataQualityCheckSingleCheck,
+];
 
-export default finalManifest;
+export const refs = Object.freeze(privateRefs);
+
+export const ajv = new Ajv({
+  allErrors: true,
+  strictTuples: false,
+  schemas: privateRefs,
+});
+
+export const validate = ajv.compile(schema);
