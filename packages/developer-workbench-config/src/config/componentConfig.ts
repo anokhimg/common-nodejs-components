e4fe0193653e0197b88e3044cd31ea2a4b34b31e
Aneree Shah <aneree.shah@accenture.com>
2023-09-05 12:47:50 +0530

    Error of columnsToDecrypt solved

--- packages/developer-workbench-config/src/config/componentConfig.ts
+++ packages/developer-workbench-config/src/config/componentConfig.ts
@@ -6075,8 +6075,6 @@ export const extract = [
             typeOfComponent: 'Extract',
             nameOfComponent: 'readDataIceberg',
             inputFieldName: 'columnsToDecrypt',
-            defaultValue: false,
-            visualType: 'checkbox',
             isOptional: true,
             isOptionalInSchema: false,
             directInput: false,
